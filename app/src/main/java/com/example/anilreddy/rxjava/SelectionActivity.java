0d1209f5c322f42c71c8c25b096fe22feaff850e
AnilRdyAleti <AnilRdyAleti@github.com>
2018-06-21 17:45:37 -0400

    Added ui for selection activity and Added dimens folder

--- app/src/main/java/com/example/anilreddy/rxjava/SelectionActivity.java
+++ app/src/main/java/com/example/anilreddy/rxjava/SelectionActivity.java
@@ -2,12 +2,31 @@ package com.example.anilreddy.rxjava;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class SelectionActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void startSearchActivity(View view) {
+    }
+
+    public void startComposeOperator(View view) {
+    }
+
+    public void startPaginationActivity(View view) {
+    }
+
+    public void startRxBusActivity(View view) {
+    }
+
+    public void startNetworkingActivity(View view) {
+    }
+
+    public void startOperatorsActivity(View view) {
     }
 }
