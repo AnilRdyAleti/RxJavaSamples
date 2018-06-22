8a3b164b80a86b5a3441cb0f67c7a0c65175945c
AnilRdyAleti <AnilRdyAleti@github.com>
2018-06-22 13:53:51 -0400

    Added Rxjava libraries and added button simple example activity

--- app/src/main/java/com/example/anilreddy/rxjava/operations/SimpleExampleActivity.java
+++ app/src/main/java/com/example/anilreddy/rxjava/operations/SimpleExampleActivity.java
@@ -2,14 +2,30 @@ package com.example.anilreddy.rxjava.operations;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.anilreddy.rxjava.R;
 
 public class SimpleExampleActivity extends AppCompatActivity {
 
+    private static final String TAG = SimpleExampleActivity.class.getName();
+    Button btn;
+    TextView textView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_simple_example);
+        btn = findViewById(R.id.btn);
+        textView = findViewById(R.id.textView);
+
+        btn.setOnClickListener((v) -> {
+            doSomeWork();
+        });
+    }
+
+    private void doSomeWork() {
+
     }
 }
