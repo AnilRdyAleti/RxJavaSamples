8d9a572a3553d1ba6d95fb96946018c7c6294531
AnilRdyAleti <AnilRdyAleti@github.com>
2018-06-22 18:05:50 -0400

    created disposable operator example

---
+++ app/src/main/java/com/example/anilreddy/rxjava/operations/DisposableExampleActivity.java
@@ -0,0 +1,15 @@
+package com.example.anilreddy.rxjava.operations;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.anilreddy.rxjava.R;
+
+public class DisposableExampleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_example_sample);
+    }
+}
