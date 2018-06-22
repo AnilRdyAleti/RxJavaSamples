4a9778b1a05910a88d0f7c20c5226d99af2bf547
AnilRdyAleti <AnilRdyAleti@github.com>
2018-06-22 18:58:46 -0400

    added take activity and worked with take operator to get exact positon

---
+++ app/src/main/java/com/example/anilreddy/rxjava/operations/TakeExampleActivity.java
@@ -0,0 +1,76 @@
+package com.example.anilreddy.rxjava.operations;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.example.anilreddy.rxjava.R;
+import com.example.anilreddy.rxjava.utils.AppConstant;
+
+import io.reactivex.Observable;
+import io.reactivex.Observer;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
+
+public class TakeExampleActivity extends AppCompatActivity {
+
+    private static final String TAG = TakeExampleActivity.class.getSimpleName();
+    Button btn;
+    TextView textView;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_example_sample);
+
+        btn = findViewById(R.id.btn);
+        textView = findViewById(R.id.textView);
+
+        btn.setOnClickListener((view) -> doSomeWork());
+    }
+
+    private void doSomeWork() {
+        getObservable().subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .take(3)
+                .subscribe(getObserver());
+    }
+
+    private Observable<Integer> getObservable() {
+        return Observable.just(1, 2, 3, 4, 5);
+    }
+
+    private Observer<Integer> getObserver() {
+        return new Observer<Integer>() {
+            @Override
+            public void onSubscribe(Disposable d) {
+                Log.d(TAG, " onSubscribe : " + d.isDisposed());
+            }
+
+            @Override
+            public void onNext(Integer value) {
+                textView.append(" onNext : value : " + value);
+                textView.append(AppConstant.LINE_SEPARATOR);
+                Log.d(TAG, " onNext value : " + value);
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                textView.append(" onError : " + e.getMessage());
+                textView.append(AppConstant.LINE_SEPARATOR);
+                Log.d(TAG, " onError : " + e.getMessage());
+            }
+
+            @Override
+            public void onComplete() {
+                textView.append(" onComplete");
+                textView.append(AppConstant.LINE_SEPARATOR);
+                Log.d(TAG, " onComplete");
+            }
+        };
+    }
+}
