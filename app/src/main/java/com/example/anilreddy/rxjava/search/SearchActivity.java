bd5878b89d1770b0fd4019f0c341a4a4df2e179d
AnilRdyAleti <AnilRdyAleti@github.com>
2018-06-25 12:41:04 -0400

    created an activity to search and used public subject

--- app/src/main/java/com/example/anilreddy/rxjava/search/SearchActivity.java
+++ app/src/main/java/com/example/anilreddy/rxjava/search/SearchActivity.java
@@ -1,15 +1,81 @@
 package com.example.anilreddy.rxjava.search;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.SearchView;
+import android.widget.TextView;
 
 import com.example.anilreddy.rxjava.R;
 
+import java.util.concurrent.TimeUnit;
+
+import io.reactivex.Observable;
+import io.reactivex.ObservableSource;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.functions.Consumer;
+import io.reactivex.functions.Function;
+import io.reactivex.functions.Predicate;
+import io.reactivex.schedulers.Schedulers;
+
 public class SearchActivity extends AppCompatActivity {
+
+    public static final String TAG = SearchActivity.class.getSimpleName();
+    private SearchView searchView;
+    private TextView textViewResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+        searchView = findViewById(R.id.searchView);
+        textViewResult = findViewById(R.id.textViewResult);
+
+        setUpSearchObservable();
+    }
+
+    @SuppressLint("CheckResult")
+    private void setUpSearchObservable() {
+
+        RxSearchObservable.fromView(searchView)
+                .debounce(300, TimeUnit.MILLISECONDS)
+                .filter(new Predicate<String>() {
+                    @Override
+                    public boolean test(String s) throws Exception {
+                        if (s.isEmpty()) {
+                            textViewResult.setText("");
+                            return false;
+                        } else {
+                            return true;
+                        }
+                    }
+                })
+                .distinctUntilChanged()
+                .switchMap(new Function<String, ObservableSource<String>>() {
+                    @Override
+                    public ObservableSource<String> apply(String s) throws Exception {
+                        return dataFromNetwork(s);
+                    }
+                })
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Consumer<String>() {
+                    @Override
+                    public void accept(String s) throws Exception {
+                        textViewResult.setText(s);
+                    }
+                });
+    }
+
+    private Observable<String> dataFromNetwork(final String s) {
+        return Observable.just(true)
+                .delay(2, TimeUnit.SECONDS)
+                .map(new Function<Boolean, String>() {
+
+                    @Override
+                    public String apply(Boolean aBoolean) throws Exception {
+                        return s;
+                    }
+                });
     }
 }
